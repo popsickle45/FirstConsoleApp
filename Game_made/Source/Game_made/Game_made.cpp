89a28ff9aac8816e82ac3cc9df812759ad3580a8
Umesh <umeshshretha631@gmail.com>
2026-05-21 23:05:50 +0545

    moving from UE 5.7 to UE 4

--- Game_made/Source/Game_made/Game_made.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Game_made.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_made, "Game_made" );
